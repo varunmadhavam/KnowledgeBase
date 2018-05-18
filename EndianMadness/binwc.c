d97457af05b2da608c26a9d07b152a3df9b0726b
varun mohan <varunmadhavam@gmail.com>
2018-05-18 10:04:31 +0530

    Create binwc.c

---
+++ EndianMadness/binwc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct threeNum
+{
+   char n1, n2, n3;
+};
+
+int main()
+{
+   int n;
+   struct threeNum num;
+   FILE *fptr;
+
+   if ((fptr = fopen("file.bin","wb")) == NULL){
+    printf("Error! opening file");
+      }
+       
+   num.n1 = 'a';
+   num.n2 = 'b';
+   num.n3 = 'c';
+   fwrite(&num, sizeof(struct threeNum), 1, fptr); 
+   fclose(fptr); 
+   return 0;
+}
