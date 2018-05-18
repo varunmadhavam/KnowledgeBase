ea268fa71713485732a019c85e410180e5623b51
varun mohan <varunmadhavam@gmail.com>
2018-05-18 13:54:04 +0530

    Update binwc.c

--- EndianMadness/binwc.c
+++ EndianMadness/binwc.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
-
 struct threeNum
 {
    char n1, n2, n3;
 };
-
 int main()
 {
    int n;
    struct threeNum num;
    FILE *fptr;
-
    if ((fptr = fopen("file.bin","wb")) == NULL){
     printf("Error! opening file");
       }
-       
    num.n1 = 'a';
    num.n2 = 'b';
    num.n3 = 'c';
