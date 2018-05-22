5b50d765848e011bd7e87eaf5110adc4889155dc
varun mohan <varunmadhavam@gmail.com>
2018-05-22 11:13:27 +0530

    Create getendian.c

---
+++ EndianMadness/getendian.c
@@ -0,0 +1,13 @@
+int main()
+{
+  int num=1633837924;
+  char *test;
+  test=(char*)&num;
+  if(*test=='a')
+  	printf("Big Endian Detected\n");
+  else if(*test=='d')
+  	printf("Little Endian Detected\n");
+  else
+  	printf("Unable To Detect Endianness\n");
+  return 0;
+}
