cc0a67ac45d1ec66bceff5ec0ddf3f9bfffc2abb
varun mohan <varunmadhavam@gmail.com>
2020-02-14 11:57:47 +0530

    Create firstBitSet.c

---
+++ FP/firstBitSet.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+        float f=2587;
+        int m,exp;
+        memcpy(&m, &f, 4);
+        exp = ((m >> 23) & 0xFF) - 127;
+        (f<0) ? printf("31\n") : printf("%d\n",exp);
+}
