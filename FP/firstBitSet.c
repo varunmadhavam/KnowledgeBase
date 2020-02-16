cf71a2374d8cb9bd53a5704129f166321ebb7d80
varun mohan <varunmadhavam@gmail.com>
2020-02-16 13:02:18 +0530

    Update firstBitSet.c

--- FP/firstBitSet.c
+++ FP/firstBitSet.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<string.h>
 int main()
 {
         float f=2587;
-        int m,exp;
+        int m,x;
         memcpy(&m, &f, 4);
         x = ((m >> 23) & 0xFF) - 127;
         (f<0) ? printf("31\n") : printf("%d\n",x);
