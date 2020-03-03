202b9dc818328531563fd83c0ec4292e07406fd6
varun mohan <varunmadhavam@gmail.com>
2020-03-03 13:57:44 +0530

    Update d2br.c

--- Dec2Bin/d2br.c
+++ Dec2Bin/d2br.c
@@ -15,7 +15,8 @@ if(d==1) // Recussion ends when the input is one, ie we have reached the end of 
  printf("1");
  return;
 }
-// Before printing, call the function recursively. This ensures the printing of the result in the proper order ie bottoms up.
+// Before printing, call the function recursively. This ensures the printing of the result in the proper order ie bottoms up after
+// repeated division.
 conv(d>>1); // Right shift once == divide by 2
-printf("%d",d&1); // d%n == d&(n-1) if n%2 == 0 => d%2 == d&(2-1) == m&1 ;https://stackoverflow.com/questions/11040646/faster-modulus-in-c-c
+printf("%d",d&1); // Quicker way of checking d%2. Just check the last bit of d. If it's one, then d is odd and %2 is one else zero.
 }
