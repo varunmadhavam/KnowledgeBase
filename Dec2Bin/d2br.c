9b1f90637caa7878751e109c7b41faabe9edd23d
varun mohan <varunmadhavam@gmail.com>
2020-03-03 14:00:28 +0530

    Update d2br.c

--- Dec2Bin/d2br.c
+++ Dec2Bin/d2br.c
@@ -18,5 +18,6 @@ if(d==1) // Recussion ends when the input is one, ie we have reached the end of 
 // Before printing, call the function recursively. This ensures the printing of the result in the proper order ie bottoms up after
 // repeated division.
 conv(d>>1); // Right shift once == divide by 2
-printf("%d",d&1); // Quicker way of checking d%2. Just check the last bit of d. If it's one, then d is odd and %2 is one else zero.
+printf("%d",d&1); // Quicker way of checking d%2. Just check the last bit of d. If it's 1, then d is odd and d%2 is 1 
+                  // else d is even and d%2 is 0.
 }
