6c37b6f894590934ddb8a444e5da5c0e3e664580
varun mohan <varunmadhavam@gmail.com>
2020-03-03 12:16:57 +0530

    Update d2br.c
    Added comments

--- Dec2Bin/d2br.c
+++ Dec2Bin/d2br.c
@@ -10,11 +10,12 @@ return 0;
 
 void conv(unsigned int d)
 {
-if(d==1)
+if(d==1) // Recussion ends when the input is one, ie we have reached the end of the repeated division.
 {
  printf("1");
  return;
 }
-conv(d>>1);
-printf("%d",d&1); //https://stackoverflow.com/questions/11040646/faster-modulus-in-c-c
+// Before printing, call the function recursively. This ensures the printing of the result in the proper order ie bottoms up.
+conv(d>>1); // Right shift once == divide by 2
+printf("%d",d&1); // d%n == d&(n-1) if n%2 == 0 => d%2 == d&(2-1) == m&1 ;https://stackoverflow.com/questions/11040646/faster-modulus-in-c-c
 }
